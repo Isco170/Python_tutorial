53322bc031d15c8b7c20b5daec9c66e97b5f3bc8
Francisco <franciscodamana@gmail.com>
2021-04-04 19:41:52 +0200

    Random Number

--- numbers.py
+++ numbers.py
@@ -54,25 +54,31 @@
 # Type Conversion
 # You can convert from one type to another with the int(), float(), and complex() methods:
 
-x = 1 #int
-y = 2.8 #float
-z = 1j #complex
+# x = 1 #int
+# y = 2.8 #float
+# z = 1j #complex
 
 #convert from int to float:
-a = float(x)
+# a = float(x)
 
 #convert from float to int
-b = int(y)
+# b = int(y)
 
 #convert from int to complex
-c = int(x)
+# c = int(x)
 
-print(a)
-print(b)
-print(c)
+# print(a)
+# print(b)
+# print(c)
 
-print(type(a))
-print(type(b))
-print(type(c))
+# print(type(a))
+# print(type(b))
+# print(type(c))
 
 # Note: You cannot convert complex numbers into number type
+
+# Random Number
+# Python does not have a random() function to make a random number, but Python has a built-in module called random that can be used to make random numbers:
+
+import random
+print(random.randrange(1, 10))
