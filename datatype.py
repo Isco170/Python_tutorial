f28fa4b8a6f7b39ee49c7c30ac6126373bc5e87e
Francisco <franciscodamana@gmail.com>
2021-04-04 19:14:58 +0200

    Setting the Specific Data Type

--- datatype.py
+++ datatype.py
@@ -12,17 +12,33 @@ x = 5
 print(type(x))
 
 # Setting the Data Type
-a = "Hello World"
-b = 20
-c = 20.5
-d = 1j
-e = ["Apple", "Banana", "Cherry"]
-f = ("Apple", "Banana", "Cherry")
+# a = "Hello World"
+# b = 20
+# c = 20.5
+# d = 1j
+# e = ["Apple", "Banana", "Cherry"]
+# f = ("Apple", "Banana", "Cherry")
+# g = range(6)
+# h = {"name" : "John", "age" : 23}
+# i = {"Apple", "Banana", "Cherry"}
+# j = frozenset({"Apple", "Banana", "Cherry"})
+# k = True
+# l = b"Hello"
+# m = bytearray(5)
+# n = memoryview(bytes(5))
+
+# Setting the Specific Data Type
+a = str("Hello World")
+b = int(20)
+c = float(20.5)
+d = complex(1j)
+e = list(("Apple", "Banana", "Cherry"))
+f = tuple(("Apple", "Banana", "Cherry"))
 g = range(6)
-h = {"name" : "John", "age" : 23}
-i = {"Apple", "Banana", "Cherry"}
-j = frozenset({"Apple", "Banana", "Cherry"})
-k = True
-l = b"Hello"
+h = dict(name = "John", age = 23)
+i = set(("Apple", "Banana", "Cherry"))
+j = frozenset(("Apple", "Banana", "Cherry"))
+k = bool(5)
+l = bytes(5)
 m = bytearray(5)
 n = memoryview(bytes(5))
