4ed32a0fb136ddd2c07808cb4393c70c21d70e7e
Francisco <franciscodamana@gmail.com>
2021-04-04 19:05:16 +0200

    first commit

---
+++ datatype.py
@@ -0,0 +1,28 @@
+# Text Type: str
+#  Numeric Types: int, float, complex
+# Sequence Types: list, tuple, range
+# Mapping Type: dict
+# Set Types: set, frozenset
+# Boolean Type: bool
+# Binary Types: bytes, bytearray, memoryview
+
+
+# Getting the Data Type:
+x = 5
+print(type(x))
+
+# Setting the Data Type
+a = "Hello World"
+b = 20
+c = 20.5
+d = 1j
+e = ["Apple", "Banana", "Cherry"]
+f = ("Apple", "Banana", "Cherry")
+g = range(6)
+h = {"name" : "John", "age" : 23}
+i = {"Apple", "Banana", "Cherry"}
+j = frozenset({"Apple", "Banana", "Cherry"})
+k = True
+l = b"Hello"
+m = bytearray(5)
+n = memoryview(bytes(5))
