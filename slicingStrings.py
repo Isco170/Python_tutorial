5255a5e6afd2077bc8258c4108327d635a71c488
Francisco <franciscodamana@gmail.com>
2021-04-10 23:51:40 +0200

    Slice Strings

---
+++ slicingStrings.py
@@ -0,0 +1,18 @@
+# Get the characters from position 2 to position 5 (not included)
+# a = "Hello, World!"
+# print(b[2:5])
+
+# Slice From the Start
+#  Get the characters from the start to position 5 (not included)
+# b = "Hello, World!"
+# print(b[:5])
+
+# Slice To the End
+#  Get the characters from position 2, and all the way to the end
+# c = "Hello, World!"
+# print(c[2:])
+
+# Negative Indexing
+# Use negative indexes to start the slice from the end of the string
+# d = "Hello, World!"
+# print(d[-5:-2])
