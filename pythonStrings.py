bc0c2dfbe92753c8e03e0cbb12814ba5d050ea22
Francisco <franciscodamana@gmail.com>
2021-04-10 23:42:11 +0200

    Python Strings

---
+++ pythonStrings.py
@@ -0,0 +1,44 @@
+# Strings
+# 'hello' is the same as "hello"
+
+# a = "Hello"
+# print(a)
+
+# Multiline Strings
+
+# b = """Lorem ipsum dolor sit amet,
+# consectetur adipiscing elit,
+# sed do eiusmod tempor incididunt
+# ut labore et dolore magna aliqua. """
+# print(b)
+
+# c = '''Lorem ipsum dolor sit amet,
+# # consectetur adipiscing elit,
+# # sed do eiusmod tempor incididunt
+# # ut labore et dolore magna aliqua.'''
+# print(c)
+
+
+# Strings are Arrays
+# d = "Hello, World!"
+# print(d[1])
+
+
+# Looping Through a String
+# for x in 'banana':
+#     print(x)
+
+
+# String Length
+# e = "Hello, World!"
+# print(len(e));
+
+# Check String
+# txt = 'The best things in life are free!'
+# print("free" in txt)
+
+# if "free" in txt:
+#     print("Yes, 'free' is present.")
+
+# print("expensive" not in txt)
+
