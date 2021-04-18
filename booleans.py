81b6f9c94f95b58685bf6c83a335722a51d42e31
Francisco <franciscodamana@gmail.com>
2021-04-18 17:11:21 +0200

    Booelans

---
+++ booleans.py
@@ -0,0 +1,35 @@
+# Booleans represent one of two values: True or False.
+
+# print(10 > 9)
+# print(10 == 9)
+# print(10 < 9)
+
+# When you run a condition in an if statement, Python returns True or False:
+
+# a = 200
+# b = 33
+# if b > a:
+#     print("B is greater than A")
+# else:
+#     print("B is not greater than A")
+
+
+# Functions can Return a Boolean
+# def myFunction() :
+#   return True
+
+# print(myFunction())
+
+# You can execute code based on the Boolean answer of a function:
+# def myFunction() :
+#   return False
+
+# if myFunction():
+#   print("YES!")
+# else:
+#   print("NO!")
+
+
+# Check if an object is an integer or not:
+# x = 200
+# print(isinstance(x, int))
