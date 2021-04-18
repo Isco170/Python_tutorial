b453ac802e759f0f66c08753442d8b1cc70afd8a
Francisco <franciscodamana@gmail.com>
2021-04-18 16:25:08 +0200

    Modify Strings in Python

---
+++ Strings/modifyStrings.py
@@ -0,0 +1,25 @@
+# Upper Case
+# The upper() method returns the string in upper case:
+# a = "Hello, World!"
+# print(a.upper())
+
+# Lower Case
+# The lower() method returns the string in lower case:
+# b = "HELLO, EVERYONE!"
+# print(b.lower())
+
+# Remove whitespace
+# The strip() method removes any whitespace from the beginning or the end:
+# c = " Hello, World! "
+# print(c.strip())
+
+# Replace String
+# The replace() method replaces a string with another string:
+# d = "Francisco"
+# print(d.replace("Fra", "Bla"))
+
+# Split String
+# The split() method splits the string into substrings if it finds instances of the separator:
+# e = "Hello, World"
+# print(e.split(","))
+
