f85fdc6d5ca509fae602433eb49b176aca63b2fe
Francisco <franciscodamana@gmail.com>
2021-04-18 16:32:00 +0200

    Concatenate Strings in Python

---
+++ Strings/concatenateStrings.py
@@ -0,0 +1,13 @@
+# String Concatenation
+# To concatenate, or combine, two strings you can use the + operator
+
+# Merge variable "a" with variable "b" into variable "c":
+# a = "Hello"
+# b = "World"
+# c = a + b
+# print(c)
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
