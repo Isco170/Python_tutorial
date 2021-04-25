f60a4392b037b30f1ec54f48d44845a305b710a4
Francisco <franciscodamana@gmail.com>
2021-04-25 21:42:01 +0200

    Python Operators

---
+++ operators.py
@@ -0,0 +1,61 @@
+# python divides the operators in the following groups:
+
+# Arithmetic operators
+# Assignment operators
+# Comparison operators
+# Logical operators
+# Identity operators
+# Membership operators
+# Bitwise operators
+
+# Python Arithmetic Operators
+x = 5
+y = 2
+
+# print(x + y) #Addition
+# print(x - y) #Subtraction
+# print(x * y) #Multiplication
+# print(x / y) #Division
+# print(x % y) #Modulus
+# print(x ** y) #Exponentiation
+# print(x // y) #Floor division
+
+# Python Assignment Operators
+# x = 7
+# x += 3
+# x -= 3
+# x *= 3
+# x /= 3
+# x %= 3
+# x //= 3
+# x **= 3
+
+# Python Comparison Operators
+#  x == y #Equal
+#  x != y #Not equal
+#  x > y #Greater than
+#  x < y #Less than
+#  x >= y #Greater than or equal to
+#  x <= y #Less than or equal to
+
+# Python Logical Operators => Logical operators are used to combine conditional statements
+
+# and #(Returns True if both statements are true)
+# or #(Returns True if one of the statements is true)
+# not #(Reverse the result, returns False if the result is true)
+
+# Python Identity Operators => are used to compare the objects, not if they are equal, but if they are actually the same object, with the same memory location:
+
+# x is y #(Returns True if both variables are the same object)
+# x is not y #(Returns True if both variables are not the same object)
+
+# Python Membership Operators => are used to test if a sequence is presented in an object:
+
+# x = 'Francisco'
+# y = 'isco'
+
+# x in y #(Returns True if a sequence with the specified value is present in the object)
+
+# x not in y #(Returns True if a sequence with the specified values is not present in the object)
+
+
