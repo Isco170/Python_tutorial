733951073a41e7b0420bed9ef8db4f9027d0fca3
Francisco <franciscodamana@gmail.com>
2021-06-15 16:57:39 +0200

    Change a range of item values

--- Lists/changeItemValue.py
+++ Lists/changeItemValue.py
@@ -1,3 +1,9 @@
-thislist = ["Apple", "Banana", "Cherry"]
-thislist[1] = "Blackcurrant"
+#thislist = ["Apple", "Banana", "Cherry"]
+#thislist[1] = "Blackcurrant"
+#print(thislist)
+
+#Change a range of item values
+thislist = ["Apple", "Banana", "Cherry", "Orange", "Kiwi", "Mango"]
+thislist[1:3] = ["Blackcurrant", "Watermelon"]
 print(thislist)
+
