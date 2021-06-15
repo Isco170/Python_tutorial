78c125e84a122e170075d5a7340a7aa2dee06061
Francisco <franciscodamana@gmail.com>
2021-06-15 17:04:40 +0200

    Done

--- Lists/changeItemValue.py
+++ Lists/changeItemValue.py
@@ -3,8 +3,12 @@
 #print(thislist)
 
 #Change a range of item values
-thislist = ["Apple", "Banana", "Cherry", "Orange", "Kiwi", "Mango"]
-print(thislist)
-thislist[1:3] = ["Blackcurrant", "Watermelon"]
-print(thislist)
+#thislist = ["Apple", "Banana", "Cherry", "Orange", "Kiwi", "Mango"]
+#print(thislist)
+#thislist[1:3] = ["Blackcurrant", "Watermelon"]
+#print(thislist)
 
+#Insert new value to the list insert() - inserts item at the specified index
+thislist = ["Apple", "Banana", "Cherry"]
+thislist.insert(2, "Watermelon")
+print(thislist)
