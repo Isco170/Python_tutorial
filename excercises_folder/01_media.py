ce6689409d50c5aced3f06cbb4e39689850ff83a
Francisco <franciscodamana@gmail.com>
2021-06-15 16:14:41 +0200

    Fazendo exercicio da area.

--- excercises_folder/01_media.py
+++ excercises_folder/01_media.py
@@ -1,6 +1,7 @@
-nota1 = input("Digite a primeira nota: ")
-nota2 = input("Digite a segunda nota: ")
-
+#nota1 = input("Digite a primeira nota: ")
+#nota2 = input("Digite a segunda nota: ")
+nota1 = 17
+nota2 = 18
 media = (int(nota1)+ int(nota2))/2
 
 #Converti media em string, porque nao aceita concatenar float com string para imprimir
---
+++ excercises_folder/02_areaQuadrado.py
@@ -0,0 +1,5 @@
+lado = input("Digite a medida de um dos lados do quadrado: ")
+
+area = int(lado**lado)
+
+print("Area: " + area)
