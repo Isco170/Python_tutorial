a3dd96da092b0ab5a40dccf0f8baeb11238f84ee
Francisco <franciscodamana@gmail.com>
2021-06-15 17:07:34 +0200

    Done

--- Lists/changeItemValue.py
+++ Lists/changeItemValue.py
@@ -9,6 +9,15 @@
 #print(thislist)
 
 #Insert new value to the list insert() - inserts item at the specified index
-thislist = ["Apple", "Banana", "Cherry"]
-thislist.insert(2, "Watermelon")
-print(thislist)
+#thislist = ["Apple", "Banana", "Cherry"]
+#thislist.insert(2, "Watermelon")
+#print(thislist)
+
+#Append Items
+#To add an item to the end of the list, use the append() method:
+
+# thislist = ["Apple", "Banana", "Cherry"]
+# thislist.append("Orange")
+# print(thislist)
+
+
