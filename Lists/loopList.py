2aa626e86af48490eadf7fe3a458f594704345b1
Francisco <franciscodamana@gmail.com>
2021-06-15 17:29:49 +0200

    Loop through index

--- Lists/loopList.py
+++ Lists/loopList.py
@@ -1,3 +1,10 @@
+#thislist = ["Apple", "Banana", "Cherry"]
+#for x in thislist:
+#   print(x)
+
+#Loop through the index numbers
+#Use the range() and len() functions to create a suitable iterable
+
 thislist = ["Apple", "Banana", "Cherry"]
-for x in thislist:
-    print(x)
+
+
