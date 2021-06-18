809bafc6e1b526a26693f4cc8b6a07d120852af6
Francisco <franciscodamana@gmail.com>
2021-06-18 21:51:14 +0200

    Return values

--- Functions/01.py
+++ Functions/01.py
@@ -3,9 +3,17 @@
 
 # my_function()
 
-def my_function(fname):
-    print(fname + " Refsnes")
+# def my_function(fname):
+#     print(fname + " Refsnes")
 
-my_function("Emil")
-my_function("Tobias")
-my_function("Linus")
+# my_function("Emil")
+# my_function("Tobias")
+# my_function("Linus")
+
+#Return Values
+def my_function(x):
+    return 5 * x
+
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
